a71857f8a71fc4c906e2ce3e92fe08d28b81054c
metdata04 <metdata04@gmail.com>
2024-10-25 12:19:03 +0530

    committed

--- daily_Tmin_graph.py
+++ daily_Tmin_graph.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
-# Load the rainfall data from the CSV file
-csv_file_path = 'extracted_data/metstation_rainfall.csv'  
+# Load the Tmin data from the CSV file
+csv_file_path = 'extracted_data/metstation_tmin_data.csv'  
 df = pd.read_csv(csv_file_path)
 
 # Convert the 'Date' column to datetime format
@@ -18,14 +18,14 @@ thirty_days_ago = current_date - timedelta(days=30)
 # Filter data for the past 30 days
 df_last_30_days = df[(df['Date'] >= thirty_days_ago) & (df['Date'] <= current_date)]
 
-# Plot daily average rainfall as a bar chart
+# Plot daily average Tmin as a bar chart
 plt.figure(figsize=(12, 6))
-plt.bar(df_last_30_days['Date'], df_last_30_days['Average Rainfall'], color='blue', width=0.8, label='Average Rainfall')
+plt.bar(df_last_30_days['Date'], df_last_30_days['Average Tmin'], color='purple', width=0.8, label='Average Tmin')
 
 # Customize the plot
-plt.title('Daily Average Rainfall Over the Past 30 Days', fontsize=14, weight='bold')
+plt.title('Daily Average Tmin Over the Past 30 Days', fontsize=14, weight='bold')
 plt.xlabel('Date', fontsize=12)
-plt.ylabel('Average Rainfall (mm)', fontsize=12)
+plt.ylabel('Average Tmin (°C)', fontsize=12)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 # Set x-axis ticks to show every date with proper rotation
@@ -33,7 +33,7 @@ plt.xticks(df_last_30_days['Date'], df_last_30_days['Date'].dt.strftime('%m/%d/%
 
 # Generate a unique filename based on the current date and time
 current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
-filename = f'Graphs/Avg_RF/daily_rainfall_average_past_30_days_{current_time}.png'
+filename = f'Graphs/Avg_Tmin/daily_tmin_average_past_30_days_{current_time}.png'
 
 # Save the plot with the unique filename
 plt.tight_layout()
